6756b5b38cf70022274831a48a3acd030494b133
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-23 14:15:59 -0600

    update YDI

---
+++ YDI Page 58/YDI Page 58/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YDI_Page_58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anInt = -123;
+            uint anUnsignedInt = 567;
+
+            Console.WriteLine("The int is {0} and the unsigned int is {1}.",
+                anInt, anUnsignedInt);
+        }
+    }
+}
