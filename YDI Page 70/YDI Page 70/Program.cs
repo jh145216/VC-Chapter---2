bb87619173290d35e5e4a501b2e0ac2d0950ef53
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-23 14:30:22 -0600

    update YDI

---
+++ YDI Page 70/YDI Page 70/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YDI_Page_70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 4;
+            bool isSixMore = 6 > value;
+            Console.WriteLine("When value is {0} isSixMore is {1}",
+                value, isSixMore);
+            value = 35;
+            isSixMore = 6 > value;
+            Console.WriteLine("When value is {0} isSixMore is {1}",
+                value, isSixMore);
+        }
+    }
+}
