40797a388a8a15b07365388d22363e1514e778fd
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-23 14:26:03 -0600

    update YDI

---
+++ YDI Page 67/YDI Page 67/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YDI_Page_67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 43, value2 = 10,
+                sum, diff, product, quotient, remainder;
+
+            sum = value1 + value2;
+            diff = value1 - value2;
+            product = value1 * value2;
+            quotient = value1 / value2;
+            remainder = value1 % value2;
+
+            Console.WriteLine("The sum of {0} and {1} is {2}",
+                value1, value2, sum);
+            Console.WriteLine("The difference between {0} and {1}" +
+                "is {2}", value1, value2, diff);
+            Console.WriteLine("The product of {0} and {1} is {2}",
+                value1, value2, product);
+            Console.WriteLine("{0} divided by {1} is {2}", value1, 
+                value2, quotient);
+            Console.WriteLine("and the remainder is {0}", remainder);
+        }
+    }
+}
