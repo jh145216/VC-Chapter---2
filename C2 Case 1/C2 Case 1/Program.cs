d686d7161409b940d3e1d908dbeac724224fcf70
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-30 14:38:33 -0600

    update case1

---
+++ C2 Case 1/C2 Case 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2_Case_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************");
+            Console.WriteLine("* The stars shine in Greenville *");
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Each contestant pays a $25 entrance fee");
+        }
+    }
+}
